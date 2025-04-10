6753422831fc9238f94582d28278d204582231ef
anneporcia <annporcia@hotmail.com>
2025-04-10 09:55:10 +0100

    Time: 768 ms (73.28%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,8 @@
 /* Write your T-SQL query statement below */
 select
-    max(e1.name) as name
+    e1.name as name
 from Employee e1
 inner join Employee e2
     on e1.id=e2.managerId
-group by e1.id
+group by e1.id, e1.name
 having count(e1.id)>=5
