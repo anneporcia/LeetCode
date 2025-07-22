5ef7c2480f88fadade337a981332dd14f11a8bde
anneporcia <annporcia@hotmail.com>
2025-07-22 14:21:25 +0100

    Time: 3083 ms (21.12%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -3,7 +3,7 @@ select
     r.contest_id 
     ,cast((cast(count(distinct r.user_id) as decimal(10,2))/cast((select count(distinct u.user_id) from Users u) as decimal(10,2))*100) as decimal(10,2)) as percentage
 from Register r
-left join Users u
+join Users u
     on u.user_id=r.user_id
 group by r.contest_id
 order by percentage desc, r.contest_id asc
