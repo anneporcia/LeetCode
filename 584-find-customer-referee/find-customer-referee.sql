a4decb3fe98101440c2bf974735b2c8f4f896605
anneporcia <annporcia@hotmail.com>
2025-07-27 23:19:07 +0100

    Time: 481 ms (49.31%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,6 +1,6 @@
-/* Write your T-SQL query statement below */
-select 
+# Write your MySQL query statement below
+select
     name
-from customer
-where referee_id != 2
+from Customer
+where referee_id <> "2"
     or referee_id is null
